699aab77f3addac8cb8223b6455d0070e3505d13
epik7th <epik7th@gmail.com>
2024-11-07 10:25:13 +0500

    add normalize.css and switch to css module

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,30 @@
 import { useState } from 'react'
 import ReactLogo from './assets/react.svg?react'
 import viteLogo from '/vite.svg'
-import './App.scss'
+import styles from './App.module.scss'
 import * as React from 'react'
 
 export const App: React.FC = () => {
 	const [count, setCount] = useState(0)
 
 	return (
-		<>
+		<div className={styles.root}>
 			<div>
 				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
+					<img src={viteLogo} className={styles.logo} alt="Vite logo" />
 				</a>
 				<a href="https://react.dev" target="_blank">
-					<ReactLogo className="logo react" />
+					<ReactLogo className={`${styles.logo} ${styles.react}`} />
 				</a>
 			</div>
 			<h1>Vite + React</h1>
-			<div className="card">
+			<div className={styles.card}>
 				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
 				<p>
 					Edit <code>src/App.tsx</code> and save to test HMR
 				</p>
 			</div>
-			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-		</>
+			<p className={styles.readTheDocs}>Click on the Vite and React logos to learn more</p>
+		</div>
 	)
 }
