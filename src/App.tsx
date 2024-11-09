d1161ba122df434d21f9a747b7576fb69c1f4b9d
epik7th <epik7th@gmail.com>
2024-11-10 00:13:01 +0500

    completed task

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,70 @@
-import { useState } from 'react'
-import ReactLogo from './assets/react.svg?react'
-import viteLogo from '/vite.svg'
 import styles from './App.module.scss'
 import * as React from 'react'
+import { useState } from 'react'
+
+interface ListItem {
+	id: number
+	value: string
+	createdAt: Date
+}
 
 export const App: React.FC = () => {
-	const [count, setCount] = useState(0)
+	const [value, setValue] = useState<string>('')
+	const [list, setList] = useState<ListItem[]>([])
+	const [error, setError] = useState<string>('')
+	const [isValueValid, setIsValueValid] = useState<boolean>(false)
 
+	const addValueToList = (value: string) => {
+		setList((updatedList) => [...updatedList, { id: Date.now(), value, createdAt: new Date() }])
+	}
+
+	const onInputButtonClick = () => {
+		const promptValue: string | null = prompt('Введите значение')
+		if (promptValue && promptValue.trim().length >= 3) {
+			setValue(promptValue)
+			setError('')
+			setIsValueValid(true)
+		} else {
+			setError('Введенное значение должно содержать минимум 3 символа')
+			setIsValueValid(false)
+		}
+	}
+	const onAddButtonClick = () => {
+		if (isValueValid) {
+			setValue('')
+			setError('')
+			setIsValueValid(false)
+			addValueToList(value)
+		}
+	}
 	return (
-		<div className={styles.root}>
-			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className={styles.logo} alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<ReactLogo className={`${styles.logo} ${styles.react}`} />
-				</a>
+		<div className={styles.app}>
+			<h1 className={styles.pageHeading}>Ввод значения</h1>
+			<p className={styles.noMarginText}>
+				Текущее значение <code>value</code>: "<output className={styles.currentValue}>{value}</output>"
+			</p>
+			{error !== '' && <div className={styles.error}>{error}</div>}
+			<div className={styles.buttonsContainer}>
+				<button className={styles.button} onClick={onInputButtonClick}>
+					Ввести новое
+				</button>
+				<button className={styles.button} onClick={onAddButtonClick} disabled={!isValueValid}>
+					Добавить в список
+				</button>
 			</div>
-			<h1>Vite + React</h1>
-			<div className={styles.card}>
-				<button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
+			<div className={styles.listContainer}>
+				<h2 className={styles.listHeading}>Список:</h2>
+				{list.length === 0 && <p className={styles.noMarginText}>Нет добавленных элементов</p>}
+				{list.length > 0 && (
+					<ul className={styles.list}>
+						{list.map((item) => (
+							<li key={item.id} className={styles.listItem}>
+								{item.value} - {item.createdAt.toLocaleDateString()} {item.createdAt.toLocaleTimeString()}
+							</li>
+						))}
+					</ul>
+				)}
 			</div>
-			<p className={styles.readTheDocs}>Click on the Vite and React logos to learn more</p>
 		</div>
 	)
 }
